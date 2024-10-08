e09fbb8e2363070e219ef97622b8f8e9f5b068a7
sydorovay <sydorovay@gmail.com>
2024-10-08 22:35:31 +0200

    update Operation.js

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,8 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { setCurrentUser, loginSuccess  } from './authSlice';
+import { setCurrentUser, loginSuccess, logoutSuccess } from './authSlice';
 
 axios.defaults.baseURL = 'https://connections-api.goit.global/';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 export const register = createAsyncThunk(
   'auth/register',
@@ -16,15 +17,39 @@ export const login = createAsyncThunk(
   'auth/login',
   async (credentials, { dispatch }) => {
     const response = await axios.post('/users/login', credentials);
+    // Зберігаємо токен у заголовках для подальших запитів
+    axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
     dispatch(loginSuccess(response.data));
     return response.data;
   }
 );
-
 export const logout = createAsyncThunk(
   'auth/logout',
-  async () => {
+  async (_, { dispatch }) => {
     await axios.post('/users/logout');
+    // Видаляємо токен з заголовків після виходу
+    delete axios.defaults.headers.common.Authorization;
+    dispatch(logoutSuccess());
+  }
+);
+
+export const refreshUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, { getState }) => {
+    const state = getState();
+    const token = state.auth.token;
+
+    if (!token) {
+      return;
+    }
+
+    try {
+      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+      const response = await axios.get('/users/current');
+      return response.data;
+    } catch (error) {
+      console.error('Error refreshing user:', error);
+    }
   }
 );
 
@@ -49,19 +74,5 @@ export const getCurrentUserThunk = () => async (dispatch, getState) => {
   }
 };
 
-export const refreshUser = createAsyncThunk(
-  'auth/refresh',
-  async (_, { getState }) => {
-    const state = getState();
-    const token = state.auth.token;
 
-    if (!token) {
-      return;
-    }
-
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    const response = await axios.get('/users/current');
-    return response.data;
-  }
-);
 
--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.goit.global';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
   try {
