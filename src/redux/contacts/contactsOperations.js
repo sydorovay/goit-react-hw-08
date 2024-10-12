6818c8d4159bcd0f68aec6e4b6552d79aebf36b8
sydorovay <sydorovay@gmail.com>
2024-10-12 22:32:40 +0200

    fix

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -4,8 +4,17 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
+  const state = thunkAPI.getState();
+  const token = state.auth.token;
+
+  if (!token) {
+    return thunkAPI.rejectWithValue('No token');
+  }
+
   try {
-    const response = await axios.get('/contacts');
+    const response = await axios.get('/contacts', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
