0e93ce6fc9733de08af7e127f55f78a2b03fce6f
sydorovay <sydorovay@gmail.com>
2024-10-13 19:48:28 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getCurrentUserThunk } from './redux/auth/authOperations';
 import Layout from './components/Layout/Layout';
-import HomePage from './pages/HomePage/HomePage';
-import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import ContactsPage from './pages/ContactsPage/ContactsPage';
+import HomePage from './ціу5рц5ер/HomePage/HomePage';
+import RegistrationPage from './ціу5рц5ер/RegistrationPage/RegistrationPage';
+import LoginPage from './ціу5рц5ер/LoginPage/LoginPage';
+import ContactsPage from './ціу5рц5ер/ContactsPage/ContactsPage';
 import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
