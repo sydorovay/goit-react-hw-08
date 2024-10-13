c1861bf7a9b399c3a1653219ce530b0e784ca409
sydorovay <sydorovay@gmail.com>
2024-10-13 21:05:36 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,19 @@
+
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/contactsOperations';
 import css from './Contact.module.css';
-
 const Contact = ({ contact }) => {
   const dispatch = useDispatch();
-  
   const handleDelete = (id) => {
     dispatch(deleteContact(id));
-  };
-
+};
   return (
     <li className={css.contactItem}>
       <span>{contact.name}: {contact.number}</span>
-      <button onClick={() => handleDelete(contact.id)} className={css.deleteButton}>
+      <button onClick={handleDelete} className={css.deleteButton}>
         Delete
       </button>
     </li>
   );
 };
-
 export default Contact;
